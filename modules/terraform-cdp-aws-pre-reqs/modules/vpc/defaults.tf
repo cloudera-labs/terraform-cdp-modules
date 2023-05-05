92952af1b7c108a33211d6595b724209cedd2a5d
Jim Enright <jenright@cloudera.com>
2023-05-05 19:34:24 +0100

    Exclude unsupported AZs during auto-VPC creation
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- modules/terraform-cdp-aws-pre-reqs/modules/vpc/defaults.tf
+++ modules/terraform-cdp-aws-pre-reqs/modules/vpc/defaults.tf
@@ -13,10 +13,16 @@
 # limitations under the License.
 
 locals {
+
+  azs_to_exclude = [ "us-east-1e"] # List of AWS AZs which are not supported by CDP
+
+    # Create a list of supported zones in the region
+  zones_in_region = tolist(setsubtract(data.aws_availability_zones.zones_in_region.names, local.azs_to_exclude))
+  
   # ------- Determine subnet details from inputs -------
   subnets_required = {
-    total   = (var.deployment_template == "public") ? length(data.aws_availability_zones.zones_in_region.names) : 2 * length(data.aws_availability_zones.zones_in_region.names)
-    public  = length(data.aws_availability_zones.zones_in_region.names)
-    private = (var.deployment_template == "public") ? 0 : length(data.aws_availability_zones.zones_in_region.names)
+    total   = (var.deployment_template == "public") ? length(local.zones_in_region) : 2 * length(local.zones_in_region)
+    public  = length(local.zones_in_region)
+    private = (var.deployment_template == "public") ? 0 : length(local.zones_in_region)
   }
 }
