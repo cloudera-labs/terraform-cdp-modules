446042a3de4982199dcd14e407c163ee064a0b83
Jim Enright <jenright@cloudera.com>
2023-05-16 19:27:35 +0100

    Update tf fmt
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- modules/terraform-cdp-aws-pre-reqs/variables.tf
+++ modules/terraform-cdp-aws-pre-reqs/variables.tf
@@ -160,8 +160,8 @@ variable "datalake_version" {
   description = "The Datalake Runtime version. Valid values are semantic versions, e.g. 7.2.16"
 
   validation {
-     condition     = (var.datalake_version == null ? true : length(regexall("\\d+\\.\\d+.\\d+", var.datalake_version)) > 0)
-     error_message = "Valid values for var: datalake_version must match semantic versioning conventions."
+    condition     = (var.datalake_version == null ? true : length(regexall("\\d+\\.\\d+.\\d+", var.datalake_version)) > 0)
+    error_message = "Valid values for var: datalake_version must match semantic versioning conventions."
   }
 
   default = "7.2.16"
