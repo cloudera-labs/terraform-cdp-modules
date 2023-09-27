37462356bed4d7552a06aca32d2d35c8e94878a3
Jim Enright <jenright@cloudera.com>
2023-09-27 20:17:01 +0100

    Update README and fix fmt in cdp-deploy module
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- modules/terraform-cdp-deploy/modules/aws/main.tf
+++ modules/terraform-cdp-deploy/modules/aws/main.tf
@@ -124,8 +124,8 @@ resource "cdp_datalake_aws_datalake" "cdp_datalake" {
   datalake_name    = var.datalake_name
   environment_name = cdp_environments_aws_environment.cdp_env.environment_name
 
-  instance_profile        = var.idbroker_instance_profile_arn
-  storage_location_base   = var.data_storage_location
+  instance_profile      = var.idbroker_instance_profile_arn
+  storage_location_base = var.data_storage_location
 
   runtime           = var.datalake_version == "latest" ? null : var.datalake_version
   scale             = var.datalake_scale
