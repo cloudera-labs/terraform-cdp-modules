c2ab00ef011cb9641ca8dab696fbb2da7d2006cc
Jim Enright <jenright@cloudera.com>
2024-06-10 16:50:25 +0100

    Update AWS provider version (#66)
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- modules/terraform-cdp-aws-pre-reqs/provider.tf
+++ modules/terraform-cdp-aws-pre-reqs/provider.tf
@@ -16,7 +16,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.67.0"
+      version = ">= 5.30"
     }
     http = {
       source  = "hashicorp/http"
