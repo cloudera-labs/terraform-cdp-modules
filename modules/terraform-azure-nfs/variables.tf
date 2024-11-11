032715d88656a9c14c206480f2a41e8e4d809f22
Jim Enright <jenright@cloudera.com>
2024-11-11 19:24:30 +0000

    Add variable validation to Azure nfs module
    Signed-off-by: Jim Enright <jenright@cloudera.com>

--- modules/terraform-azure-nfs/variables.tf
+++ modules/terraform-azure-nfs/variables.tf
@@ -24,7 +24,6 @@ variable "vnet_name" {
 
 }
 
-
 variable "azure_region" {
   type        = string
   description = "Region for CDP"
@@ -40,6 +39,16 @@ variable "private_endpoint_prefix" {
 variable "nfs_file_share_name" {
   type        = string
   description = "nfs file share name"
+
+  validation {
+    condition     = length(var.nfs_file_share_name) >= 3 && length(var.nfs_file_share_name) <= 63
+    error_message = "The length of nfs_file_share_name must be between 3 and 63 characters."
+  }
+
+  validation {
+    condition     = can(regex("^[a-z0-9-]{1,63}$", var.nfs_file_share_name))
+    error_message = "nfs_file_share_name can consist only of lowercase letters, numbers, hyphens (-)."
+  }
 }
 
 
@@ -51,6 +60,15 @@ variable "nfs_private_endpoint_target_subnet_names" {
 variable "nfs_storage_account_name" {
   type        = string
   description = "NFS Storage account name"
+
+  validation {
+    condition     = (length(var.nfs_storage_account_name) >= 3 && length(var.nfs_storage_account_name) <= 24)
+    error_message = "The length of nfs_storage_account_name must be between 3 and 24 characters."
+  }
+  validation {
+    condition     = (can(regex("^[a-z0-9]{1,24}$", var.nfs_storage_account_name)))
+    error_message = "nfs_storage_account_name can consist only of lowercase letters and numbers."
+  }
 }
 
 variable "nfs_file_share_size" {
@@ -69,24 +87,64 @@ variable "nfsvm_public_ip_name" {
   type        = string
   description = "Name for NFS VM Public IP"
   default     = null
+
+  validation {
+    condition     = (var.nfsvm_public_ip_name == null ? true : length(var.nfsvm_public_ip_name) >= 1 && length(var.nfsvm_public_ip_name) <= 80)
+    error_message = "The length of nfsvm_public_ip_name must be 80 characters or less."
+  }
+
+  validation {
+    condition     = (var.nfsvm_public_ip_name == null ? true : can(regex("^[a-zA-Z0-9.-_]{1,80}$", var.nfsvm_public_ip_name)))
+    error_message = "nfsvm_public_ip_name can consist only of letters, numbers, dots (.), hyphens (-) and underscores (_)."
+  }
 }
 
 variable "nfsvm_nic_name" {
   type        = string
   description = "Name for NFS VM NIC"
   default     = null
+
+  validation {
+    condition     = (var.nfsvm_nic_name == null ? true : length(var.nfsvm_nic_name) >= 1 && length(var.nfsvm_nic_name) <= 80)
+    error_message = "The length of nfsvm_nic_name must be 80 characters or less."
+  }
+
+  validation {
+    condition     = (var.nfsvm_nic_name == null ? true : can(regex("^[a-zA-Z0-9.-_]{1,80}$", var.nfsvm_nic_name)))
+    error_message = "nfsvm_public_ip_name can consist only of letters, numbers, dots (.), hyphens (-) and underscores (_)."
+  }
 }
 
 variable "nfsvm_sg_name" {
   type        = string
   description = "Name for NFS VM Security Group"
   default     = null
+
+  validation {
+    condition     = (var.nfsvm_sg_name == null ? true : length(var.nfsvm_sg_name) >= 1 && length(var.nfsvm_sg_name) <= 80)
+    error_message = "The length of nfsvm_sg_name must be 80 characters or less."
+  }
+
+  validation {
+    condition     = (var.nfsvm_sg_name == null ? true : can(regex("^[a-zA-Z0-9.-_]{1,80}$", var.nfsvm_sg_name)))
+    error_message = "nfsvm_sg_name can consist only of letters, numbers, dots (.), hyphens (-) and underscores (_)."
+  }
 }
 
 variable "nfsvm_name" {
   type        = string
   description = "Name for NFS VM"
   default     = null
+
+  validation {
+    condition     = (var.nfsvm_name == null ? true : length(var.nfsvm_name) >= 1 && length(var.nfsvm_name) <= 64)
+    error_message = "The length of nfsvm_name must be 64 characters or less."
+  }
+
+  validation {
+    condition     = (var.nfsvm_name == null ? true : can(regex("^[a-zA-Z0-9.-_]{1,64}$", var.nfsvm_name)))
+    error_message = "nfsvm_name can consist only of letters, numbers, dots (.), hyphens (-) and underscores (_)."
+  }
 }
 
 variable "public_key_text" {
