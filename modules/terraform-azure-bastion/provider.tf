c49b4a79ad50ef96643a7b00b28718873e4f50bb
dennaloh <dennaloh@hotmail.com>
2025-04-16 16:58:20 +0800

    Update provider with tls

--- modules/terraform-azure-bastion/provider.tf
+++ modules/terraform-azure-bastion/provider.tf
@@ -18,6 +18,14 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 4.0.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.5.1"
+    }
   }
 
   required_version = ">= 1.9.0"
