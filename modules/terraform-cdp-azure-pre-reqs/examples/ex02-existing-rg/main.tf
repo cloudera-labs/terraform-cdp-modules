1d7ef655ebaecc65e8a3378f82ae06e9b5b883a5
Jim Enright <jenright@cloudera.com>
2025-09-30 20:52:47 +0100

    Add support for pre-existing resource group (#120)
    Signed-off-by: Jim Enright <jenright@cloudera.com>

---
+++ modules/terraform-cdp-azure-pre-reqs/examples/ex02-existing-rg/main.tf
@@ -0,0 +1,52 @@
+# Copyright 2023 Cloudera, Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+
+}
+
+provider "azuread" {
+
+}
+
+# ------- Azure Resource Group -------
+resource "azurerm_resource_group" "rmgp" {
+
+  name     = "${var.env_prefix}-existing-rg"
+  location = var.azure_region
+
+  tags = { Name = "${var.env_prefix}-existing-rg" }
+}
+
+module "ex04_existing_rg" {
+  source = "../.."
+
+  env_prefix   = var.env_prefix
+  azure_region = var.azure_region
+
+  deployment_template = var.deployment_template
+
+  ingress_extra_cidrs_and_ports = var.ingress_extra_cidrs_and_ports
+
+  # Used existing resource group
+  create_resource_group  = false
+  cdp_resourcegroup_name = azurerm_resource_group.rmgp.name
+
+  depends_on = [azurerm_resource_group.rmgp]
+}
